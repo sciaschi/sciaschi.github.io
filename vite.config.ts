cecaebc6395a3f1dddf3795ba4d7ac81e2433cc7
Sean Ciaschi <sciaschi1@gmail.com>
2025-05-21 15:51:16 -0400

    added base for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sciaschi.github.io/',
   plugins: [
     vue(),
     vueJsx(),
