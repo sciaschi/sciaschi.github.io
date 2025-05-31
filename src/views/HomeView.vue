200dfe0aebdf002052241e11f59755e62c8a942c
Sean Ciaschi <sciaschi1@gmail.com>
2025-05-31 00:02:22 -0400

    Added job history section on home page, fixed styling for skills

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,8 +10,17 @@ const skillsList = [
   "CSS",
   "Javascript",
   "Node.js",
-  "MySQL"
-]
+  "MySQL",
+  "Docker",
+  "ExpressJS",
+  "Git",
+  "Linux",
+  "MSSQL",
+  "Microsoft Office",
+  "TypeScript",
+];
+
+skillsList.sort();
 </script>
 
 <template>
@@ -19,17 +28,16 @@ const skillsList = [
     <div id="info-content">
       <v-img :src="meImg" rounded="circle" cover width="248" height="248" class="mx-15"></v-img>
       <v-container fluid id="description">
-        Hello, my name is Sean Ciaschi! I’m a web developer who
-        loves building things from the ground up. A lot of the time, you’ll find me hacking together
-        websites, embedded systems, designing PCBs, or working on some other sort of electronics project.
-        I’m especially into projects that blur the line between software and hardware and push the bounds of
-        current technologies.
+        Hello, my name is Sean Ciaschi! I’m a web developer wholoves building things from the ground
+        up. A lot of the time, you’ll find me hacking together websites, embedded systems, designing
+        PCBs, or working on some other sort of electronics project. I’m especially into projects that
+        blur the line between software and hardware and push the bounds of current technologies.
       </v-container>
     </div>
   </v-sheet>
   <v-container fluid>
     <v-row>
-      <v-col>
+      <v-col cols="6">
         <v-card>
           <v-card-title class="pa-5" style="font-size:x-large;"><v-icon icon="mdi-account-school" /> Education</v-card-title>
           <v-card-text>
@@ -44,13 +52,90 @@ const skillsList = [
         </v-card>
       </v-col>
 
-      <v-col>
+      <v-col cols="6">
         <v-card>
           <v-card-title class="pa-5" style="font-size:x-large;"><v-icon icon="mdi-laptop" /> Skills</v-card-title>
           <v-card-text>
-            <div class="d-flex ga-2">
-                <v-chip v-for="(skill, index) in skillsList" :key="index">{{skill}}</v-chip>
+            <div class="ga-2">
+                <v-chip v-for="(skill, index) in skillsList" :key="index" class="ma-1">{{skill}}</v-chip>
             </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title class="pa-5" style="font-size:x-large;"><v-icon icon="mdi-laptop" /> Job History</v-card-title>
+          <v-card-text>
+            <v-timeline direction="horizontal">
+              <v-timeline-item>
+                <template v-slot:opposite>
+                  January 2017 - August 2019
+                </template>
+                <div>
+                  <div class="text-h6 pb-4">FasTrax Solutions, Rock Hill, NY — Web Developer</div>
+                  <p>
+                    Developed Point of Sale (POS) and business management software using
+                    the Laravel PHP framework to support retail operations and backend
+                    administration.
+                  </p>
+                </div>
+              </v-timeline-item>
+
+              <v-timeline-item>
+                <template v-slot:opposite>
+                  August 2019 - October 2020
+                </template>
+                <div>
+                  <div class="text-h6 pb-4">Greenshades Software, Remote — Junior Software Developer</div>
+                  <p>
+                    Designed and developed an employee management system to
+                    streamline HR operations and general business workflows.
+                  </p>
+                </div>
+              </v-timeline-item>
+
+              <v-timeline-item>
+                <template v-slot:opposite>
+                  October 2021 – March 2022
+                </template>
+                <div>
+                  <div class="text-h6 pb-4">Home Depot, Matamoras, PA — Overnight Freight Associate</div>
+                  <p>
+                    Responsible for unloading, stocking, and organizing merchandise in a retail or
+                    warehouse environment during overnight hours. Ensures shelves are replenished
+                    and inventory is accurate, while maintaining a clean and safe workspace.
+                  </p>
+                </div>
+              </v-timeline-item>
+              <v-timeline-item>
+                <template v-slot:opposite>
+                  June 2022 – August 2022
+                </template>
+                <div>
+                  <div class="text-h6 pb-4">Unrelated, Taxi Service — Big Run, PA — Driver</div>
+                  <p>
+                    Provides transportation services to passengers, ensuring timely and safe travel
+                    to destinations. Responsibilities include navigating routes, maintaining vehicle
+                    cleanliness, and offering excellent customer service.
+                  </p>
+                </div>
+              </v-timeline-item>
+              <v-timeline-item>
+                <template v-slot:opposite>
+                  August 2022 – Present
+                </template>
+                <div>
+                  <div class="text-h6 pb-4">Movate, Remote — Game Content Moderator</div>
+                  <p>
+                    Review and evaluate user-submitted usernames to ensure
+                    compliance with community guidelines, terms of service, and content
+                    policies.
+                  </p>
+                </div>
+              </v-timeline-item>
+            </v-timeline>
           </v-card-text>
         </v-card>
       </v-col>
